d6f88eabd4cbc9d9159c6c46335676385af29022
Gustavo Lopes <gustavolop@gmail.com>
2026-03-11 10:53:54 -0300

    exercicios feitos até módulo 2.8 criando uma agenda de telefones, verificando se não há registro duplicado sem atualizar a página

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      number: '040-1234567'
+    }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    
+    if(persons.find(
+      item => JSON.stringify(item) === JSON.stringify(personObject)
+    )){
+      window.alert(`${newName} is already added to phonebook`)
+    }else{
+      setPersons(persons.concat(personObject))
+    }
+    
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => { 
+   // console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => { 
+   // console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: 
+          <input 
+          value={newName}
+          onChange={handleNameChange}/>
+        </div>
+        <div>number: 
+          <input value={newNumber}
+          onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <p>{person.name} {person.number}</p>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
