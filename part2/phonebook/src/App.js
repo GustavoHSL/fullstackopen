03f6c85d19eb79a73727ff7b19ac82cb6f84fb90
Gustavo Lopes <gustavolop@gmail.com>
2026-03-11 14:41:28 -0300

    separa os componentes Filter, PersonForm e Persons

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,69 @@
 import { useState } from 'react'
 
+  const Filter = ({value, onChange}) => {
+
+    return (
+      <div>
+        filter shown with:
+          <input 
+            value={value}
+            onChange={onChange}/>
+      </div>
+    )
+  }
+
+  const PersonForm = ({onSubmit, newName, nameOnChange, newNumber, numberOnChange}) => {
+    return (
+        <form onSubmit={onSubmit}>
+          <div>
+            name: 
+            <input 
+            value={newName}
+            onChange={nameOnChange}/>
+          </div>
+          <div>number: 
+            <input value={newNumber}
+            onChange={numberOnChange}/>
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+      </form>
+    )
+  }
+
+  const Persons = ({personsToShow}) => {
+    return (
+      <div>
+        <ul>
+        {personsToShow.map(person =>
+          <p key={person.id}>{person.name} {person.number}</p>
+        )}
+      </ul>
+      </div>
+    )
+  }
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [showFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length+1
     }
 
-    
     if(persons.find(
       item => JSON.stringify(item) === JSON.stringify(personObject)
     )){
@@ -30,39 +77,44 @@ const App = () => {
   }
 
   const handleNameChange = (event) => { 
-   // console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => { 
-   // console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+
+  const handleFilterChange = (event) => { 
+    setNewFilter(event.target.value)
+  }
+
+    const personsToShow = showFilter
+    ? persons.filter(person => person.name.toLowerCase().includes(showFilter.toLowerCase()))
+    : persons
+
+
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: 
-          <input 
-          value={newName}
-          onChange={handleNameChange}/>
-        </div>
-        <div>number: 
-          <input value={newNumber}
-          onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
-          <p>{person.name} {person.number}</p>
-        )}
-      </ul>
+
+      <Filter value={showFilter} onChange={handleFilterChange} />  
+
+      <h3>adda a new</h3>
+
+      <PersonForm 
+        onSubmit={addPerson} 
+        newName={newName} 
+        nameOnChange={handleNameChange}
+        newNumber={newNumber}
+        numberOnChange={handleNumberChange} 
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons personsToShow={personsToShow} />
+
     </div>
   )
 }
