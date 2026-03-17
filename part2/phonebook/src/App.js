7ec7000a3d52d6d201a9b5c77059816cf81a27d3
Gustavo Lopes <gustavolop@gmail.com>
2026-03-17 00:25:12 -0300

    feat: integra phonebook com backend - adiciona persistência, serviço de API, exclusão e atualização de contatos

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,79 +1,51 @@
-import { useState } from 'react'
-
-  const Filter = ({value, onChange}) => {
-
-    return (
-      <div>
-        filter shown with:
-          <input 
-            value={value}
-            onChange={onChange}/>
-      </div>
-    )
-  }
-
-  const PersonForm = ({onSubmit, newName, nameOnChange, newNumber, numberOnChange}) => {
-    return (
-        <form onSubmit={onSubmit}>
-          <div>
-            name: 
-            <input 
-            value={newName}
-            onChange={nameOnChange}/>
-          </div>
-          <div>number: 
-            <input value={newNumber}
-            onChange={numberOnChange}/>
-          </div>
-          <div>
-            <button type="submit">add</button>
-          </div>
-      </form>
-    )
-  }
-
-  const Persons = ({personsToShow}) => {
-    return (
-      <div>
-        <ul>
-        {personsToShow.map(person =>
-          <p key={person.id}>{person.name} {person.number}</p>
-        )}
-      </ul>
-      </div>
-    )
-  }
+import { useState, useEffect} from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Person from './components/Person'
+import personsService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    personsService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length+1
+      //id: persons.length+1
     }
+    const existingPerson = persons.find(item => item.name === newName)
 
-    if(persons.find(
-      item => JSON.stringify(item) === JSON.stringify(personObject)
-    )){
-      window.alert(`${newName} is already added to phonebook`)
+    if(existingPerson){
+      if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
+        personsService.update(existingPerson.id, personObject).then(returnedPerson => {
+        setPersons(persons.map(p => p.id !== existingPerson.id ? p : returnedPerson))
+          setNewName('')
+        setNewNumber('')
+        })
+      //window.alert(`${newName} is already added to phonebook`)
+        }
     }else{
-      setPersons(persons.concat(personObject))
+      personsService
+        .create(personObject)
+        .then(returnedPersons => {
+            setPersons(persons.concat(returnedPersons))
+            setNewName('')
+            setNewNumber('')
+      })
     }
-    
-    setNewName('')
-    setNewNumber('')
   }
 
   const handleNameChange = (event) => { 
@@ -92,9 +64,15 @@ const App = () => {
     ? persons.filter(person => person.name.toLowerCase().includes(showFilter.toLowerCase()))
     : persons
 
-
+  const deletePerson = (person) => {
+    if(window.confirm(`Delete ${person.name} ?`)){
+      personsService.remove(person.id).then(() => {
+        setPersons(persons.filter(p => p.id !== person.id))
+      })
+    }
+    
+  }
   
-
   return (
     <div>
       <h2>Phonebook</h2>
@@ -113,7 +91,10 @@ const App = () => {
 
       <h3>Numbers</h3>
 
-      <Persons personsToShow={personsToShow} />
+        {personsToShow.map(person => 
+          <Person key={person.id} person={person} deleteP={() => deletePerson(person)}/>
+        )}
+ 
 
     </div>
   )
