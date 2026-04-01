cc831efbb7dc3bdd1c07451b8fbd61244743926c
Gustavo Lopes <gustavolop@gmail.com>
2026-04-01 07:18:23 -0300

    feat: add /info endpoint to provide phonebook information and current date

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -32,6 +32,13 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const currentDate = new Date().toLocaleString();
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+        <p>${currentDate} (${timeZone})</p>`)
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
