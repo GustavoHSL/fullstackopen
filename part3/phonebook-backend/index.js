4a8fa94dd4006b7f9bedfb798b0442d2f4e1605a
Gustavo Lopes <gustavolop@gmail.com>
2026-04-02 09:40:30 -0300

    projeto notes da part2 copiado para part3 para utilizar em paralelo ao notes backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 morgan.token('body', (request) => {
     if(request.method === 'POST') {
@@ -10,7 +11,7 @@ morgan.token('body', (request) => {
 })
 
 app.use(express.json())
-
+app.use(cors())
 
 //utiliza o formato do tiny e acrescenta o body quando for a situação de POST
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -107,7 +108,7 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
