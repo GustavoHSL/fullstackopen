cc5499ef140124812f1b911926b5e3c7dae64e0a
Gustavo Lopes <gustavolop@gmail.com>
2026-04-06 14:54:32 -0300

    fix: update backend to serve static files and adjust frontend API base URL

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-
+app.use(express.static('build'))
 morgan.token('body', (request) => {
     if(request.method === 'POST') {
         return JSON.stringify(request.body)
